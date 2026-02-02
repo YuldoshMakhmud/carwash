3e8d4d26181032cf627a1664747e7a4d5bc96814
YuldoshMakhmud <yuldoshmakhmud@gmail.com>
2026-02-02 17:03:46 +0900

    texts

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Car Wash',
+      title: '프리미엄 세차',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.red,
